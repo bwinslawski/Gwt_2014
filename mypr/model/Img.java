07dbfb1eba50389d306d2987609a031736f797b6
bwinslawski <wisla162@gmail.com>
2014-06-07 10:57:31 +0200

    gwt

---
+++ mypr/model/Img.java
@@ -0,0 +1,34 @@
+package e.g.com.example.mypr.model;
+
+public class Img {
+	private int id = 1;
+	private String path= "siemka";
+	private String country= "elooo";
+	private boolean was= false;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public boolean isWas() {
+		return was;
+	}
+	public void setWas(boolean was) {
+		this.was = was;
+	}
+
+	
+}
